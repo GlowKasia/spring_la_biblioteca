ea13168b2aae60c7ddf244fe6862f032dcbab7fc
GlowKasia <gwkasix@gmail.com>
2019-10-02 18:37:18 +0200

    firsts

---
+++ src/main/java/com/sdajava25/spring_la_biblioteca/SpringLaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.sdajava25.spring_la_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLaBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdajava25/spring_la_biblioteca/SpringLaBibliotecaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdajava25.spring_la_biblioteca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLaBibliotecaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
